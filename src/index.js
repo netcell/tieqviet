6f8f0b266755269be4ecacae74fa7872dbe98407
Nguyễn Tuấn Anh <anhnt.fami@gmail.com>
2017-11-25 16:58:41 +0700

    Update example

--- src/index.js
+++ src/index.js
@@ -27,15 +27,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: `
-LUẬT GIÁO DỤC <Điều 7. Ngôn ngữ dùng trong nhà trường và cơ sở giáo dục khác; dạy và học tiếng nói, chữ viết của dân tộc thiểu số; dạy ngoại ngữ.
-
-1. Tiếng Việt là ngôn ngữ chíh thức dùng trong nhà trường và cơ sở giáo dục khác. Căn cứ vào mục tiêu giáo dục và yêu cầu cụ thể về nội dung giáo dục, Thủ tướg chíh phủ quy định việc dạy và học bằng tiếng nước ngoài trong nhà trường và cơ sở giáo dục khác.
-
-2. Nhà nước tạo điều kiện để người dân tộc thiểu số được học tiếng nói, chữ viết của dân tộc mình nhằm giữ gìn và phát huy bản sắc văn hoá dân tộc, giúp cho học sinh người dân tộc thiểu số dễ dàng tiếp thu kiến thức khi học tập trong nhà trường và cơ sở giáo dục khác. Việc dạy và học tiếng nói, chữ viết của dân tộc thiểu số được thực hiện theo quy định của Chíh phủ.
-
-3. Ngoại ngữ quy định trong chương trình giáo dục là ngôn ngữ được sử dụng phổ biến trong giao dịch quốc tế. Việc tổ chức dạy ngoại ngữ trong nhà trường và cơ sở giáo dục khác cần đảm bảo để người học được học liên tục và có hiệu quả.
-`
+      value: 'Con chó tru'
     };
 
     this.handleChange = this.handleChange.bind(this);
